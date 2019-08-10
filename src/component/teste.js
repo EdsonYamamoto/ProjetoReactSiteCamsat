b38c4f2a505ba3dcb2819494fb205294394e2b2a
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2019-08-10 15:51:37 -0300

    commit inicial

---
+++ src/component/teste.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ComponentTeste extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>asdnaksdk</h1>
+            </div>
+        );
+    }
+}
